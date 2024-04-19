2fd8912046972eb2ef7d5766cf8e033d01bc88de
mfrankl <mfrankl@seibert-media.net>
2024-04-19 10:54:50 +0200

    anzeige der Nachrichten

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,63 @@
 "use client";
 import GCChatProvider from "@/components/chat-provider";
+import { NewChat } from "@/components/new-chat";
+import { useEffect, useState } from "react";
+import { Conversation, StoredChatMessage } from "@/types/types";
+import { Timestamp } from "@google-cloud/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { frontendService } from "@/lib/frontend/FrontendService";
+import { firestoreDb } from "@/lib/frontend/Firebase";
+
+const emptyConversation: Conversation = {
+  chatOwner: {
+    name: "Geochacher",
+    id: "You",
+    avatar: "",
+  },
+  chatPartner: {
+    name: "Tim",
+    id: "Tim",
+    avatar: "/tim.svg",
+  },
+  messages: [],
+};
 
 export default function ChatPage() {
+  const [conversation, setConversation] = useState<Conversation>();
+
+  useEffect(() => {
+    const conv = emptyConversation;
+
+    const cachesession = frontendService.getCachesession();
+    const conversationId = cachesession;
+    if (!cachesession || !conversationId) {
+      throw new Error("no cachesession");
+    }
+    const collectionReference = collection(
+      firestoreDb,
+      `/cache-sessions/${cachesession}/chat`,
+    );
+    const queryRef = query(collectionReference, orderBy("created"));
+    const unsubscribe = onSnapshot(queryRef, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        const newMessages = snapshot
+          .docChanges()
+          .filter((change) => change.type === "added")
+          .map((change) => change.doc.data() as StoredChatMessage);
+
+        if (newMessages.length > 0) {
+          setConversation((prevConversation) => ({
+            ...(prevConversation || emptyConversation),
+            messages: [...(prevConversation?.messages || []), ...newMessages],
+          }));
+        }
+      });
+    });
+    setConversation(conv);
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div
       style={{
@@ -11,7 +67,13 @@ export default function ChatPage() {
         width: "100%",
       }}
     >
-      <GCChatProvider></GCChatProvider>
+      {conversation && (
+        <NewChat
+          sendChatMessage={async (message) => {}}
+          conversation={conversation}
+        ></NewChat>
+      )}
+      {/*<GCChatProvider></GCChatProvider>*/}
     </div>
   );
 }
--- src/components/new-chat.tsx
+++ src/components/new-chat.tsx
@@ -1,5 +1,5 @@
 import "./new-chat.scss";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Conversation } from "@/types/types";
 
 type ChatParameter = {
@@ -9,6 +9,7 @@ type ChatParameter = {
 
 export function NewChat({ sendChatMessage, conversation }: ChatParameter) {
   const [message, setMessage] = useState("");
+  const [firstDate, setFirstDate] = useState<Date | null>();
 
   async function submitForm(e: FormEvent) {
     console.log("formevent", e);
@@ -17,57 +18,28 @@ export function NewChat({ sendChatMessage, conversation }: ChatParameter) {
     setMessage("");
   }
 
+  useEffect(() => {
+    if (conversation.messages.length > 0) {
+      setFirstDate(new Date(conversation.messages[0].created.toDate()));
+    }
+  }, [conversation]);
+
   return (
     <div className="center">
-      {/*<div className="contacts">*/}
-      {/*  <i className="fas fa-bars fa-2x"></i>*/}
-      {/*  <h2>Contacts</h2>*/}
-      {/*  <div className="contact">*/}
-      {/*    <div className="pic rogers"></div>*/}
-      {/*    <div className="badge">14</div>*/}
-      {/*    <div className="name">Steve Rogers</div>*/}
-      {/*    <div className="message">That is America's ass 🇺🇸🍑</div>*/}
-      {/*  </div>*/}
-      {/*  <div className="contact">*/}
-      {/*    <div className="pic stark"></div>*/}
-      {/*    <div className="name">Tony Stark</div>*/}
-      {/*    <div className="message">*/}
-      {/*      Uh, he's from space, he came here to steal a necklace from a wizard.*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*  <div className="contact">*/}
-      {/*    <div className="pic banner"></div>*/}
-      {/*    <div className="badge">1</div>*/}
-      {/*    <div className="name">Bruce Banner</div>*/}
-      {/*    <div className="message">There's an Ant-Man *and* a Spider-Man?</div>*/}
-      {/*  </div>*/}
-      {/*  <div className="contact">*/}
-      {/*    <div className="pic thor"></div>*/}
-      {/*    <div className="name">Thor Odinson</div>*/}
-      {/*    <div className="badge">3</div>*/}
-      {/*    <div className="message">I like this one</div>*/}
-      {/*  </div>*/}
-      {/*  <div className="contact">*/}
-      {/*    <div className="pic danvers"></div>*/}
-      {/*    <div className="badge">2</div>*/}
-      {/*    <div className="name">Carol Danvers</div>*/}
-      {/*    <div className="message">*/}
-      {/*      Hey Peter Parker, you got something for me?*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
       <div className="chat">
         <div className="contact bar">
           <div
             className="pic"
             style={{
-              backgroundImage: 'url("/tim.svg")',
+              backgroundImage: `url("${conversation.chatPartner.avatar}")`,
             }}
           ></div>
           <div className="name">{conversation.chatPartner.name}</div>
         </div>
         <div className="messages" id="chat">
-          <div className="time">Today at 11:41</div>
+          {firstDate && (
+            <div className="time">{firstDate.toLocaleString()}</div>
+          )}
           {conversation.messages.map((message) => (
             <div
               className={`message ${message.sender === conversation.chatOwner.id ? "outgoing" : "incoming"}`}
