e222bab03e796fe755d66f4d3d2c3fb1e4077876
mfrankl <mfrankl@seibert-media.net>
2024-04-19 14:42:42 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { frontendService } from "@/lib/frontend/FrontendService";
+import "./page.scss";
 
 export default function Home() {
   const router = useRouter();
@@ -50,13 +51,15 @@ export default function Home() {
   }
 
   return (
-    <div>
-      Enter start code or your already started session code
-      <div>
-        <input onChange={(e) => setCode(e.target.value)} />
-        <button onClick={sendCode}>send</button>
+    <div className="wrapper">
+      <div className="card">
+        Enter start code or your already started session code
+        <div>
+          <input onChange={(e) => setCode(e.target.value)} />
+          <button onClick={sendCode}>send</button>
+        </div>
+        {/*<ChatWrapper></ChatWrapper>*/}
       </div>
-      {/*<ChatWrapper></ChatWrapper>*/}
     </div>
   );
 }
