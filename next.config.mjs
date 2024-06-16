64befea56de1312a25692ec8ec584776b50459b1
mfrankl <mfrankl@seibert-media.net>
2024-06-16 17:09:21 +0200

    ignore functions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	webpack: (config, { isServer }) => {
+		// Ignore /functions directory
+		config.module.rules.push({
+			test: /\.(js|ts)$/,
+			include: [path.resolve(__dirname, 'functions')],
+			use: 'ignore-loader'
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
