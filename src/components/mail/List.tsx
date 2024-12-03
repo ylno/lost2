86802e9029d5deaae2660a82eaa2b028a1e5b0e5
mfrankl <michael.frankl@seibert.grooup>
2024-12-03 09:36:04 +0100

    set read/unread state

--- src/components/mail/List.tsx
+++ src/components/mail/List.tsx
@@ -17,7 +17,10 @@ export default function List({ emails }: ListProps) {
       <ul role="list" className="divide-y divide-gray-100">
         {emails.map((email) => (
           <li key={email.id} className="flex justify-between gap-x-6 py-5">
-            <div className="flex min-w-0 gap-x-4">
+            <div
+              className={`flex-shrink-0 w-2 h-2 ${email.unread && "bg-blue-500"} rounded-full mt-2}`}
+            ></div>
+            <div className="flex-auto min-w-0 gap-x-4">
               <div className="min-w-0 flex-auto">
                 <p className="text-sm/6 font-semibold text-gray-900">
                   <a
@@ -48,21 +51,6 @@ export default function List({ emails }: ListProps) {
                 <p className="text-sm/6 text-gray-900">
                   {dayjs(email.date).format("DD.MM.YYYY HH:mm")}
                 </p>
-                {/*{email.lastSeen ? (*/}
-                {/*  <p className="mt-1 text-xs/5 text-gray-500">*/}
-                {/*    Last seen{" "}*/}
-                {/*    <time dateTime={email.lastSeenDateTime}>*/}
-                {/*      {email.lastSeen}*/}
-                {/*    </time>*/}
-                {/*  </p>*/}
-                {/*) : (*/}
-                {/*  <div className="mt-1 flex items-center gap-x-1.5">*/}
-                {/*    <div className="flex-none rounded-full bg-emerald-500/20 p-1">*/}
-                {/*      <div className="size-1.5 rounded-full bg-emerald-500" />*/}
-                {/*    </div>*/}
-                {/*    <p className="text-xs/5 text-gray-500">Online</p>*/}
-                {/*  </div>*/}
-                {/*)}*/}
               </div>
               <Menu as="div" className="relative flex-none">
                 <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
